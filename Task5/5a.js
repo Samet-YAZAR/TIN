d7b9ecdf138f89ec8f3c07956a880438e6c297bc
yzrsamet <yzr.samet@gmail.com>
2020-11-25 18:15:50 +0100

    5a is composed and tested.

--- Task5/5a.js
+++ Task5/5a.js
@@ -1,25 +1,23 @@
 //
 
-let person = {
+let player = {
   firstName:'Samet',
   lastName: 'YAZAR',
-  dateofBirth : "1996/03/25",
+  dateofBirth : new Date("1996/03/25"),
   age : function() {
-      var birth =this.dateofBirth.split('-');
-      // Please pay attention to the month (parts[1]); JavaScript counts months from 0:
-      // January - 0, February - 1, etc.
-      let today = new Date().toISOString().slice(0, 10);
-      return today[0]-birth[0];
+      var currentDate = new Date();
+     return currentDate.getFullYear() - dateofBirth.getFullYear();
   },
 
   adulthood : function () {
-    return  (age > 18 ? "Adult": "Child");
+    return  (this.age > 18 ? "Adult": "Child");
   }
 
 }
  function printProperties(object) {
    for(let pName in object) {
      let property = object[pName];
-     console.log("name" + pName + "\nvalue" + property + "\ntype" + typeof property + "\n" );
+     console.log("name : " + pName + "\nvalue : " + property + "\ntype : " + typeof property + "\n" );
    }
  }
+printProperties(player);
