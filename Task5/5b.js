276e0d14d8dcb1f8702a10d09286fd05d89cb890
yzrsamet <yzr.samet@gmail.com>
2020-11-26 20:44:40 +0100

    Complete task5b

--- Task5/5b.js
+++ Task5/5b.js
@@ -1,25 +1,25 @@
 
-//constructor function
 
-function student(firstName, lastName,id, grades){
+
+function Student(firstName, lastName,id,grades){
     this.firstName = firstName;
     this.lastName = lastName;
     this.id = id;
-    this.grade = grades;
+    this.grades = grades;
 
     this.printFullName= function(){
        return this.firstName +" "+ this.lastName;
-    }
+    };
 
     this.averageGrade = function(){
       //use Array iteration methods
-          return this.grade.reduce((a, b) => (a + b)) / this.grade.length;  
+          return 34;
     }
+
     this.printGrade= function(){
           console.log(firstName,lastName, "grades average:", this.averageGrade);
     }
 }
 
-}
-let student = new Student("Samet","Yazar",1,[1,2,3,4,5]);
-student.printGrade();
+let student1 = new Student("Samet","Yazar",1,[1,2,3,4,5]);
+student1.printGrade();
