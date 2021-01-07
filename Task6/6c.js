a9e501d41e5441f3b0bcd996e74cdc57b49f5ec6
yzrsamet <yzr.samet@gmail.com>
2021-01-07 23:42:48 +0100

    Complete task6c

--- Task6/6c.js
+++ Task6/6c.js
@@ -1,16 +1,37 @@
 
-function validate()
+function validateForm(){
 
-
-function emailIsValid (email) {
-  return /\S+@\S+\.\S+/.test(email)
+  if(!validateName(name[0].value)){
+    document.getElementsByClassName("warningMessage")[0].style.display= "block";
+  }
+  if(!validateName(name[1].value)){
+    document.getElementsByClassName("warningMessage")[1].style.display= "block";
+  }
+  if(!validateEmail(email.value)){
+    document.getElementsByClassName("warningMessage")[2].style.display= "block";
+  }
+  alert(validateName(name[0].value));
+  alert(validateName(name[1].value));
+  alert(validateName(email.value));
 }
 
-function numberValid(){
-    var z = document.forms["myForm"]["num"].value;
-    if(!z.match(/^\d+/)) {
-        alert("Please only enter numeric characters only for your phone! (Allowed input:0-9)")
-        return false;
-        }
-    return true;
+const email = document.getElementById('email');
+const number = document.getElementById('phone');
+const name = document.getElementsByClassName('name');
+const okSubmit = document.getElementById('submit');
+
+
+okSubmit.addEventListener('click',function(e){
+    e.preventDefault();
+    validateForm();
+});
+
+function validateName(name){
+  var letters = /^[A-Za-z]+$/;
+  return letters.test(name);
+}
+
+function validateEmail(email){
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
